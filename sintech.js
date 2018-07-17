a62e10c70138d31855f7510e3dbd99472ddb3581
Naufale <41264190+Naufale@users.noreply.github.com>
2018-07-17 21:47:59 +0700

    Update sintech.js

--- sintech.js
+++ sintech.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on("ready", async () => {
-	console.log('Aku Hidup!'); // untuk memberitahu bahwa bot sudah siap
+	console.log('Ready!'); // untuk memberitahu bahwa bot sudah siap
 });
 
 bot.on("message", async (message) => {
