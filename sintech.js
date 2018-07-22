4d8b4af4810b6ee7cae6ecbf2b65674715f27ac7
Naufale <41264190+Naufale@users.noreply.github.com>
2018-07-22 20:03:27 +0700

    Add files via upload

--- sintech.js
+++ sintech.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js"); 
-const bot = new Discord.Client({disableEveryone: false}); // botnya tidak bisa mention ke everyone
+const bot = new Discord.Client({disableEveryone: true}); // botnya tidak bisa mention ke everyone
 const config = require("./config.json"); // tempat menaruh prefix & token
+const fs = require("fs");
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} Bot Ready!`); // untuk memberitahu bahwa bot sudah siap
@@ -10,12 +11,13 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
 	if (message.author.bot) return; // bot tidak akan menjawab apabila di command bot lain
 	if (message.channel.type === 'dm') return; // bot tidak akan menjawab di DM
-	
+
 	let prefix = config.prefix;
-	let msg = message.content.toLowerCase(); // case snsitive
-	let sender = message.autor; // pengirim
-	let args = message.content.slice(prefix.length).trim().split(" ")// bisa milih prefi/command
-	let cmd = args.shift().toLowerCase(); // command tidak case sensitive
+	let msg = message.content.toLowerCase(); // command disisipkan spasi
+	let args = message.content.slice(prefix.length).trim().split(" ");
+	let cmd = args.shift().toLowerCase();
+
+	if (!msg.startsWith(prefix)) return; // agar bot tidak baca pesan tanpa prefix
 
 	try {
 		let commandFile = require(`./cmds/${cmd}.js`);
@@ -26,6 +28,18 @@ bot.on("message", async message => {
 		console.log(`${author} menggunakan perintah ${cmd}`);
 	}
 
+});
+
+bot.on("guildMemberAdd", member => {
+	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
+	if (!autorole[member.guild.id]) { // jika tidak ada autorole yang di set, agar tidak serror saat ada yang join
+		autorole[member.guild.id] = {
+			autorole: config.autorole
+		};
+	}
+	var role = autorole[member.guild.id].role;
+	if (!role) return; // jika autorole 0 maka akan dihentikan dan tidak menyebabkan error
+	member.addRole(role);
 });
 
 bot.login(config.token);
