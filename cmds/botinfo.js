3b3d15540f582d8c2f50304676a5c6e2e232bc1b
Naufale <41264190+Naufale@users.noreply.github.com>
2018-07-26 21:51:04 +0700

    Update botinfo.js

--- cmds/botinfo.js
+++ cmds/botinfo.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 
-exports.run = async (bot, message, args) =>{
-    let bicon = bot.user.displayAvatarURL; // untuk menampikan avatar bot
+exports.run = async (client, message, args) =>{
+    let bicon = client.user.displayAvatarURL; // untuk menampikan avatar bot
     let botembed = new Discord.RichEmbed()
     .setAuthor("Bot Information")
     .setColor("#0000FF") // bisa pake kode HEX
     .setThumbnail(bicon) // thumbnail dari avatar bot
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
+    .addField("Bot Name", client.user.username)
+    .addField("Created On", client.user.createdAt);
 
     message.channel.send(botembed); // untuk mengirim embed
 }
